f7892131a931180c4006cc663ece6be9141af8b6
1ncluz1ve <acer@KDR2004.localdomain>
2025-10-26 15:33:56 +0000

    Added a new Text to get displayed

--- boot.asm
+++ boot.asm
@@ -12,10 +12,24 @@ mov si, msg
 print_loop:
     lodsb       ;Loads the next character in msg
     cmp al,0    ;Compares if al = 0 or not
-    je hang     ;if al = 0 then cpu will hang the execution of the instruction
+    je print_new_line     ;if al = 0 then cpu will hang the execution of the instruction
     int 0x10    ;BIOS Interuppt for writing character on the screen
     jmp print_loop      ;Repeat the loop
 
+print_new_line:
+    mov al,0x0A
+    int 0x10
+    mov al,0x0D
+    int 0x10
+
+mov si,real_mode_msg
+
+print_sec_msg:
+    lodsb
+    cmp al,0
+    je hang
+    int 0x10
+    jmp print_sec_msg
     
 
 hang:
@@ -23,7 +37,7 @@ hang:
     jmp hang
 
 real_mode_msg db "This is Real Mode", 0; Null terminated String
-msg db "World, World!" , 0 ; Null terminated String
+msg db "Hello, World!" , 0 ; Null terminated String
 ;Bootloader signature 
 times 510-($-$$) db 0
 dw 0xaa55
