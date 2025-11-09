5468bf429a748e2eef0f15bc1eb5a6e5907fe540
Kunal Dev Rai <kunaldevraicode@gmail.com>
2025-11-09 06:38:31 +0000

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -6,7 +6,7 @@ mov ah,0x06
 mov al,0x00
 int 0x10
 
-mov ah, 0x0e    ; The code code starts here
+mov ah, 0x0e    ; The code starts here
 mov si, msg
 
 print_loop:
@@ -27,17 +27,61 @@ mov si,real_mode_msg
 print_sec_msg:
     lodsb
     cmp al,0
-    je hang
+    je a20_enable
     int 0x10
     jmp print_sec_msg
     
+
+a20_enable:     ; This Label enable the A20 line which helps in accessing memory more that 1MB
+    in al,0x92
+    or al,2;
+    out 0x92,al
+
+load_gdt:
+    lgdt [gdt_ptr]      ; Loads the address of the gdt in the cpu register gdtr
 
 hang:
     hlt
     jmp hang
 
-real_mode_msg db "This is Real Mode", 0; Null terminated String
+real_mode_msg db "This is Real/ and After that the Protected Mode will begin", 0; Null terminated String
 msg db "Hello, World!" , 0 ; Null terminated String
+
+; Global Descriptor Table (GDT)
+gdt_start:
+  dd 0x0
+  dd 0x0
+
+gdt_code:
+  dw 0xFFFF
+  dw 0x0000
+  db 0x00
+  db 10011010b
+  db 11001111b
+  db 0x00
+
+gdt_data:
+  dw 0xFFFF
+  dw 0x0000
+  db 0x00
+  db 10010010b
+  db 11001111b
+  db 0x00
+
+gdt_end:
+
+gdt_ptr:
+  dw gdt_end - gdt_start - 1
+  dd gdt_start
+
+  
+
+
+
+
+
+
 ;Bootloader signature 
 times 510-($-$$) db 0
 dw 0xaa55
+
